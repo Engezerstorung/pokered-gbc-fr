733708ac184c56e427c87d48428a55d72aca7df0
Rangi <35663410+Rangi42@users.noreply.github.com>
2025-08-17 09:58:09 -0400

    Use `dname` macro to automatically pad names with `"@"` characters (#525)

--- data/events/trades.asm
+++ data/events/trades.asm
@@ -1,7 +1,12 @@
+MACRO npctrade
+; give mon, get mon, dialog id, nickname
+	db \1, \2, \3
+	dname \4, NAME_LENGTH
+ENDM
+
 TradeMons:
 ; entries correspond to TRADE_FOR_* constants
 	table_width 3 + NAME_LENGTH
-	; give mon, get mon, dialog id, nickname
 	; The two instances of TRADE_DIALOGSET_EVOLUTION are a leftover
 	; from the Japanese Blue trades, which used species that evolve.
 	; TRADE_DIALOGSET_EVOLUTION did not refer to evolution in Japanese
@@ -9,14 +14,14 @@ TradeMons:
 	; "went and evolved" and also changed the trades to match. English
 	; Red/Blue uses the original JP Red/Green trades but with the JP Blue
 	; post-trade text.
-	db NIDORINO,   NIDORINA,  TRADE_DIALOGSET_CASUAL,    "BIBICHE@@@@"
-	db ABRA,       MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MARCEL@@@@@"
-	db BUTTERFREE, BEEDRILL,  TRADE_DIALOGSET_HAPPY,     "CHIKUCHIKU@" ; unused
-	db PONYTA,     SEEL,      TRADE_DIALOGSET_CASUAL,    "BIBI@@@@@@@"
-	db SPEAROW,    FARFETCHD, TRADE_DIALOGSET_HAPPY,     "JULIO@@@@@@"
-	db SLOWBRO,    LICKITUNG, TRADE_DIALOGSET_CASUAL,    "GLAVIOTEUR@"
-	db POLIWHIRL,  JYNX,      TRADE_DIALOGSET_EVOLUTION, "NINI@@@@@@@"
-	db RAICHU,     ELECTRODE, TRADE_DIALOGSET_EVOLUTION, "KOURJUS@@@@"
-	db VENONAT,    TANGELA,   TRADE_DIALOGSET_HAPPY,     "BIGOUDI@@@@"
-	db NIDORAN_M,  NIDORAN_F, TRADE_DIALOGSET_HAPPY,     "FABI@@@@@@@"
+	npctrade NIDORINO,   NIDORINA,  TRADE_DIALOGSET_CASUAL,    "BIBICHE"
+	npctrade ABRA,       MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MARCEL"
+	npctrade BUTTERFREE, BEEDRILL,  TRADE_DIALOGSET_HAPPY,     "CHIKUCHIKU" ; unused
+	npctrade PONYTA,     SEEL,      TRADE_DIALOGSET_CASUAL,    "BIBI"
+	npctrade SPEAROW,    FARFETCHD, TRADE_DIALOGSET_HAPPY,     "JULIO"
+	npctrade SLOWBRO,    LICKITUNG, TRADE_DIALOGSET_CASUAL,    "GLAVIOTEUR"
+	npctrade POLIWHIRL,  JYNX,      TRADE_DIALOGSET_EVOLUTION, "NINI"
+	npctrade RAICHU,     ELECTRODE, TRADE_DIALOGSET_EVOLUTION, "KOURJUS"
+	npctrade VENONAT,    TANGELA,   TRADE_DIALOGSET_HAPPY,     "BIGOUDI"
+	npctrade NIDORAN_M,  NIDORAN_F, TRADE_DIALOGSET_HAPPY,     "FABI"
 	assert_table_length NUM_NPC_TRADES
