a90025e4afeb339afa22b4cc973028313dad57d1
Narishma-gb <194818981+Narishma-gb@users.noreply.github.com>
2025-09-03 17:56:09 +0200

    Label and constant cleanup (#528)
    * Label cleanup, add some constants instead of raw numbers
    
    * Rename load/save game functions to be more explicit

--- engine/movie/trade.asm
+++ engine/movie/trade.asm
@@ -167,7 +167,7 @@ LoadTradingGFXAndMonNames:
 	ld a, BANK(TradingAnimationGraphics2)
 	call FarCopyData2
 	ld hl, vBGMap0
-	ld bc, $800
+	ld bc, 2 * TILEMAP_AREA
 	ld a, " "
 	call FillMemory
 	call ClearSprites
@@ -226,7 +226,7 @@ Trade_ShowPlayerMon:
 	ld b, SET_PAL_POKEMON_WHOLE_SCREEN
 	ld c, 2
 	call Trade_LoadCablePalettes ; Ignores values of b, c
-	ld a, %10101011
+	ld a, LCDC_ON | LCDC_WIN_9800 | LCDC_WIN_ON | LCDC_BLOCK21 | LCDC_BG_9C00 | LCDC_OBJ_8 | LCDC_OBJ_ON | LCDC_BG_ON
 	ldh [rLCDC], a
 	ld a, $50
 	ldh [hWY], a
@@ -285,7 +285,7 @@ Trade_DrawOpenEndOfLinkCable:
 	ld a, $a0
 	ldh [hSCX], a
 	call DelayFrame
-	ld a, %10001011
+	ld a, LCDC_ON | LCDC_WIN_9800 | LCDC_WIN_OFF | LCDC_BLOCK21 | LCDC_BG_9C00 | LCDC_OBJ_8 | LCDC_OBJ_ON | LCDC_BG_ON
 	ldh [rLCDC], a
 	hlcoord 6, 2
 	ld b, TILEMAP_LINK_CABLE
@@ -344,7 +344,7 @@ Trade_AnimateBallEnteringLinkCable:
 	ld a, $1
 	ldh [hAutoBGTransferEnabled], a
 	call ClearScreen
-	ld b, $98
+	ld b, HIGH(vBGMap0)
 	call CopyScreenTileBufferToVRAM
 	call Delay3
 	xor a
@@ -458,7 +458,7 @@ Trade_InitGameboyTransferGfx:
 	ldh [hAutoBGTransferEnabled], a
 	call Trade_LoadMonPartySpriteGfx
 	call DelayFrame
-	ld a, %10101011
+	ld a, LCDC_ON | LCDC_WIN_9800 | LCDC_WIN_ON | LCDC_BLOCK21 | LCDC_BG_9C00 | LCDC_OBJ_8 | LCDC_OBJ_ON | LCDC_BG_ON
 	ldh [rLCDC], a
 	xor a
 	ldh [hSCX], a
@@ -763,7 +763,7 @@ Trade_ShowClearedWindow:
 	ld a, $1
 	ldh [hAutoBGTransferEnabled], a
 	call ClearScreen
-	ld a, %11100011
+	ld a, LCDC_DEFAULT
 	ldh [rLCDC], a
 	ld a, $7
 	ldh [rWX], a
