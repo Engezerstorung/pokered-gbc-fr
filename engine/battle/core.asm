pret/pokered

Use `ld_hli_a_string` macro to make byte-by-byte strings more obvious (#540)

engine/battle/core.asm
@@ -6841,17 +6841,7 @@ InitWildBattle:
 	ld [hli], a   ; write front sprite pointer
 	ld [hl], b
 	ld hl, wEnemyMonNick  ; set name to "GHOST"
-	ld a, 'G'
-	ld [hli], a
-	ld a, 'H'
-	ld [hli], a
-	ld a, 'O'
-	ld [hli], a
-	ld a, 'S'
-	ld [hli], a
-	ld a, 'T'
-	ld [hli], a
-	ld [hl], '@'
+	ld_hli_a_string "GHOST@"
 	ld a, [wCurPartySpecies]
 	push af
 	ld a, MON_GHOST

engine/pokemon/status_ailments.asm
@@ -10,37 +10,17 @@ PrintStatusAilment::
 	jr nz, .par
 	and SLP_MASK
 	ret z
-	ld a, 'S'
-	ld [hli], a
-	ld a, 'L'
-	ld [hli], a
-	ld [hl], 'P'
+	ld_hli_a_string "SLP"
 	ret
 .psn
-	ld a, 'P'
-	ld [hli], a
-	ld a, 'S'
-	ld [hli], a
-	ld [hl], 'N'
+	ld_hli_a_string "PSN"
 	ret
 .brn
-	ld a, 'B'
-	ld [hli], a
-	ld a, 'R'
-	ld [hli], a
-	ld [hl], 'N'
+	ld_hli_a_string "BRN"
 	ret
 .frz
-	ld a, 'F'
-	ld [hli], a
-	ld a, 'R'
-	ld [hli], a
-	ld [hl], 'Z'
+	ld_hli_a_string "FRZ"
 	ret
 .par
-	ld a, 'P'
-	ld [hli], a
-	ld a, 'A'
-	ld [hli], a
-	ld [hl], 'R'
+	ld_hli_a_string "PAR"
 	ret

home/pokemon.asm
@@ -320,11 +320,7 @@ PrintStatusCondition::
 	pop de
 	jr nz, PrintStatusConditionNotFainted
 ; if the pokemon's HP is 0, print "FNT"
-	ld a, 'F'
-	ld [hli], a
-	ld a, 'N'
-	ld [hli], a
-	ld [hl], 'T'
+	ld_hli_a_string "FNT"
 	and a
 	ret
 

Engezerstorung/pokered-gbc-fr

engine/battle/core.asm
@@ -6873,21 +6873,7 @@ InitWildBattle:
 	ld [hli], a   ; write front sprite pointer
 	ld [hl], b
 	ld hl, wEnemyMonNick  ; set name to "GHOST"
-	ld a, 'S'
-	ld [hli], a
-	ld a, 'P'
-	ld [hli], a
-	ld a, 'E'
-	ld [hli], a
-	ld a, 'C'
-	ld [hli], a
-	ld a, 'T'
-	ld [hli], a
-	ld a, 'R'
-	ld [hli], a
-	ld a, 'E'
-	ld [hli], a
-	ld [hl], '@'
+	ld_hli_a_string "SPECTRE@"
 	ld a, [wCurPartySpecies]
 	push af
 	ld a, MON_GHOST

engine/pokemon/status_ailments.asm
@@ -10,37 +10,17 @@ PrintStatusAilment::
 	jr nz, .par
 	and SLP_MASK
 	ret z
-	ld a, 'S'
-	ld [hli], a
-	ld a, 'O'
-	ld [hli], a
-	ld [hl], 'M'
+	ld_hli_a_string "SOM"
 	ret
 .psn
-	ld a, 'P'
-	ld [hli], a
-	ld a, 'S'
-	ld [hli], a
-	ld [hl], 'N'
+	ld_hli_a_string "PSN"
 	ret
 .brn
-	ld a, 'B'
-	ld [hli], a
-	ld a, 'R'
-	ld [hli], a
-	ld [hl], 'U'
+	ld_hli_a_string "BRU"
 	ret
 .frz
-	ld a, 'G'
-	ld [hli], a
-	ld a, 'E'
-	ld [hli], a
-	ld [hl], 'L'
+	ld_hli_a_string "GEL"
 	ret
 .par
-	ld a, 'P'
-	ld [hli], a
-	ld a, 'A'
-	ld [hli], a
-	ld [hl], 'R'
+	ld_hli_a_string "PAR"
 	ret

home/pokemon.asm
@@ -320,9 +320,7 @@ PrintStatusCondition::
 	pop de
 	jr nz, PrintStatusConditionNotFainted
 ; if the pokemon's HP is 0, print "KO"
-	ld a,'K'
-	ld [hli],a
-	ld [hl],'O'
+	ld_hli_a_string "KO"
 	and a
 	ret
 