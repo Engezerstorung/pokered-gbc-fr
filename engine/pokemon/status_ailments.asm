pret/pokered

Use features of RGBDS 1.0.0 (#537)

engine/pokemon/status_ailments.asm
@@ -10,37 +10,37 @@ PrintStatusAilment::
 	jr nz, .par
 	and SLP_MASK
 	ret z
-	ld a, "S"
+	ld a, 'S'
 	ld [hli], a
-	ld a, "L"
+	ld a, 'L'
 	ld [hli], a
-	ld [hl], "P"
+	ld [hl], 'P'
 	ret
 .psn
-	ld a, "P"
+	ld a, 'P'
 	ld [hli], a
-	ld a, "S"
+	ld a, 'S'
 	ld [hli], a
-	ld [hl], "N"
+	ld [hl], 'N'
 	ret
 .brn
-	ld a, "B"
+	ld a, 'B'
 	ld [hli], a
-	ld a, "R"
+	ld a, 'R'
 	ld [hli], a
-	ld [hl], "N"
+	ld [hl], 'N'
 	ret
 .frz
-	ld a, "F"
+	ld a, 'F'
 	ld [hli], a
-	ld a, "R"
+	ld a, 'R'
 	ld [hli], a
-	ld [hl], "Z"
+	ld [hl], 'Z'
 	ret
 .par
-	ld a, "P"
+	ld a, 'P'
 	ld [hli], a
-	ld a, "A"
+	ld a, 'A'
 	ld [hli], a
-	ld [hl], "R"
+	ld [hl], 'R'
 	ret

home/pokemon.asm
@@ -320,11 +320,11 @@ PrintStatusCondition::
 	pop de
 	jr nz, PrintStatusConditionNotFainted
 ; if the pokemon's HP is 0, print "FNT"
-	ld a, "F"
+	ld a, 'F'
 	ld [hli], a
-	ld a, "N"
+	ld a, 'N'
 	ld [hli], a
-	ld [hl], "T"
+	ld [hl], 'T'
 	and a
 	ret
 
@@ -337,7 +337,7 @@ PrintStatusConditionNotFainted::
 ; hl = destination address
 ; [wLoadedMonLevel] = level
 PrintLevel::
-	ld a, "<LV>" ; ":L" tile ID
+	ld a, '<LV>' ; ":L" tile ID
 	ld [hli], a
 	ld c, 2 ; number of digits
 	ld a, [wLoadedMonLevel] ; level
@@ -353,7 +353,7 @@ PrintLevel::
 ; hl = destination address
 ; [wLoadedMonLevel] = level
 PrintLevelFull::
-	ld a, "<LV>" ; ":L" tile ID
+	ld a, '<LV>' ; ":L" tile ID
 	ld [hli], a
 	ld c, 3 ; number of digits
 	ld a, [wLoadedMonLevel] ; level

Engezerstorung/pokered-gbc-fr

engine/pokemon/status_ailments.asm
@@ -10,37 +10,37 @@ PrintStatusAilment::
 	jr nz, .par
 	and SLP_MASK
 	ret z
-	ld a, "S"
+	ld a, 'S'
 	ld [hli], a
-	ld a, "O"
+	ld a, 'O'
 	ld [hli], a
-	ld [hl], "M"
+	ld [hl], 'M'
 	ret
 .psn
-	ld a, "P"
+	ld a, 'P'
 	ld [hli], a
-	ld a, "S"
+	ld a, 'S'
 	ld [hli], a
-	ld [hl], "N"
+	ld [hl], 'N'
 	ret
 .brn
-	ld a, "B"
+	ld a, 'B'
 	ld [hli], a
-	ld a, "R"
+	ld a, 'R'
 	ld [hli], a
-	ld [hl], "U"
+	ld [hl], 'U'
 	ret
 .frz
-	ld a, "G"
+	ld a, 'G'
 	ld [hli], a
-	ld a, "E"
+	ld a, 'E'
 	ld [hli], a
-	ld [hl], "L"
+	ld [hl], 'L'
 	ret
 .par
-	ld a, "P"
+	ld a, 'P'
 	ld [hli], a
-	ld a, "A"
+	ld a, 'A'
 	ld [hli], a
-	ld [hl], "R"
+	ld [hl], 'R'
 	ret

home/pokemon.asm
@@ -319,10 +319,10 @@ PrintStatusCondition::
 	or b ; is the pokemon's HP zero?
 	pop de
 	jr nz, PrintStatusConditionNotFainted
-; if the pokemon's HP is 0, print "FNT"
-	ld a,"K"
+; if the pokemon's HP is 0, print "KO"
+	ld a,'K'
 	ld [hli],a
-	ld [hl],"O"
+	ld [hl],'O'
 	and a
 	ret
 
@@ -335,7 +335,7 @@ PrintStatusConditionNotFainted::
 ; hl = destination address
 ; [wLoadedMonLevel] = level
 PrintLevel::
-	ld a, "<LV>" ; ":L" tile ID
+	ld a, '<LV>' ; ":L" tile ID
 	ld [hli], a
 	ld c, 2 ; number of digits
 	ld a, [wLoadedMonLevel] ; level
@@ -351,7 +351,7 @@ PrintLevel::
 ; hl = destination address
 ; [wLoadedMonLevel] = level
 PrintLevelFull::
-	ld a, "<LV>" ; ":L" tile ID
+	ld a, '<LV>' ; ":L" tile ID
 	ld [hli], a
 	ld c, 3 ; number of digits
 	ld a, [wLoadedMonLevel] ; level